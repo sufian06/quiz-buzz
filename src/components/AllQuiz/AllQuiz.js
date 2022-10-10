b018e4986efc6c7cac8198d618f20fccf1dd1de8
sufian <sufian4se@gmail.com>
2022-10-10 23:14:25 +0600

    all quiz show home page

--- src/components/AllQuiz/AllQuiz.js
+++ src/components/AllQuiz/AllQuiz.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+const AllQuiz = ({ quiz }) => {
+    const {logo, name} = quiz;
+  return (
+    <div className="">
+      <div className="border">
+        <img src={logo} alt="" />
+        <h4>{name}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default AllQuiz;
