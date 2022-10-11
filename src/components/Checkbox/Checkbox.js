5c1e7ac9d37d3022c150840a81ee1dd84078309d
sufian <sufian4se@gmail.com>
2022-10-11 23:52:57 +0600

    answer checked true or false

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Checkbox = ({ option, correctAnswer }) => {
-    const notify = () => toast.success("Selected ans!");
+  const handleCorrectAnswer = () => {
+    if (option === correctAnswer) {
+      toast.success("Correct Answer");
+    } else {
+      toast("Ans not Correct");
+    }
+  };
   return (
     <div>
-      <input className="mr-2" type="radio" name="option" id="" onClick={notify} />
+      <input
+        className="mr-2"
+        type="radio"
+        name="option"
+        id=""
+        onClick={handleCorrectAnswer}
+      />
       <label htmlFor="option">{option}</label>
       <ToastContainer position="top-center" />
     </div>
