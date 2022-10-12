d6691cea1da9bf12a28ad646c661c37855bf81ff
sufian <sufian4se@gmail.com>
2022-10-12 17:10:18 +0600

    chart added in statistic page

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-// import Chart from '../Chart/Chart';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const Statistics = () => {
-    const datas = useLoaderData().data;
-    console.log(datas)
-    return (
-        <div>
-            <div className='mt-28 max-w-7xl mx-auto'>
-            {/* {
-                datas.map(data => <Chart key={data.id} data={data} />)
-            } */}
-        </div>
-        </div>
-    );
+  const datas = useLoaderData().data;
+  return (
+    <div className="flex justify-center">
+      <div className="mt-28 max-w-7xl mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-12">Line Chart</h2>
+        <LineChart width={500} height={400} data={datas}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <Tooltip />
+        </LineChart>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
